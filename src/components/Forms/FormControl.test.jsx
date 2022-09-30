eec5035c027b929c6872a0c84b8538b4e1aeade0
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-09-30 15:03:51 -0600

    input control test wrote

--- src/components/Forms/FormControl.test.jsx
+++ src/components/Forms/FormControl.test.jsx
@@ -1,1 +1,18 @@
+import { render, screen } from '@testing-library/react';
+import { InputControl } from './FormControls.jsx';
 
+test('input control should work', async () => {
+  render(
+    <InputControl
+      label="color"
+      name="color"
+      required
+      placeholder="your favorite color"
+    />
+  );
+
+  const inputControl = screen.getByLabelText('color');
+  expect(inputControl.name).toEqual('color');
+  expect(inputControl.placeholder).toEqual('your favorite color');
+  expect(inputControl.required).toEqual(true);
+});
