ed6056ae8de34846cbc7e8fe2bf2232168be6021
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-09-30 18:44:42 -0600

    search renders on screen

--- src/hooks/use-search.js
+++ src/hooks/use-search.js
@@ -2,13 +2,13 @@ import { useEffect, useState } from 'react';
 import { getColorSchemes } from '../services/colors.js';
 
 export default function useSearch() {
-  const [schemes, setSchemes] = useState([]);
+  const [hexes, setHexes] = useState([]);
 
   const searchEffect = () => {
-    getColorSchemes().then(schemes => setSchemes(schemes.map(t => t.scheme)));
+    getColorSchemes().then(hexes => setHexes(hexes.map(t => t.scheme)));
   };
   useEffect(() => searchEffect(), []);
   return {
-    schemes,
+    hexes,
   };
 }
