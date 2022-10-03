e2c1a8d5dbafa1501b4499c5c48fcb6ecb5106b6
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-10-03 14:03:17 -0600

    turn in

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,48 +1,24 @@
-
-import { useState } from 'react';
 import SearchForm from './SearchForm.jsx';
-import PokemonCard from './PokeCard.jsx';
+import SearchResults from './SearchResults.jsx';
+import useSearchResults from '../../hooks/use-search-results.js';
 import { FormButton } from '../Forms/FormControl.jsx';
-import { useLocation } from 'react-router-dom';
-import { pokemonGetAll } from '../../services/pokedex.js';
-import styles from './Search.css';
 
 export default function Search() {
-  const { search } = useLocation();
-  const query = new URLSearchParams(search, [search]);
-  const type = query.get('type');
-  console.log('type', type);
-  const searchText = query.get('searchText');
-  const [page, setPage] = useState(1);
-  const [searchResults, setSearchResults] = useState([]);
-  const [formData, setFormData] = useState(null);
-  const onSubmit = async (searchFormData) => {
-    setPage(1);
-    setFormData(searchFormData);
-    const pokemonResults = await pokemonGetAll(1, searchFormData);
-    setSearchResults(pokemonResults.results);
-  };
-  const more = async () => {
-    setPage(page + 1);
-    const pokemonResults = await pokemonGetAll(page + 1, formData);
-    setSearchResults(searchResults.concat(pokemonResults.results));
-  };
+  const {
+    infiniteScrollRef,
+    nextPage,
+    pokemon,
+    setPokemon,
+    searchParams,
+    searchResults,
+    searchPokedex,
+  } = useSearchResults();
   return <section>
-    <SearchForm
-      type={type}
-      searchText={searchText}
-      onSubmit={onSubmit}
-    />
-    <ul className={styles.pokemonResultsList}>
-      {searchResults.map(result => {
-        return <li key={result._id} className={styles.searchResult}>
-          <PokemonCard pokemon={result} />
-        </li>;
-      })}
-    </ul>
-    { searchResults.length > 0
-      ? <FormButton onClick={more}>moar</FormButton>
-      : ''
-    }
+    <SearchForm pokemon={pokemon} 
+      setPokemon={setPokemon}
+      onSubmit={searchPokedex} />
+    <SearchResults results={searchResults} 
+      infiniteScrollRef={infiniteScrollRef}/>
+    <FormButton onClick={nextPage}>Moar</FormButton>
   </section>;
 }
